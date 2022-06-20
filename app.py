bf857a635b691035fc8de0a66c501ca2440f55d8
Zhaobin Zhu <zhu.zhaobin@hotmail.de>
2022-06-20 18:16:19 +0200

    add io500 endpoints

--- app.py
+++ app.py
@@ -34,6 +34,46 @@ def hello_world():  # put application's code here
     cur.connection.close()
     return json.dumps(result_list)
 
+@app.route('/iofh')
+def get_IOFHs():  # put application's code here
+    cur = get_db().cursor()
+    query = cur.execute("SELECT i.id, i.\"start\" , i.\"end\", ir.id as run_id, ir.procs , ir.version ,ir.config_hash, ir.result_dir , ir.mode, is2.MD , is2.BW , is2.SCORE , s.name, s.kernel_version ,s.processor_architecture , s.processor_model ,s.processor_frequency ,s.processor_threads, s.processor_vendor, s.processor_L2 , s.processor_L3 , s.processor_coresPerSocket , s.distribution ,s.distribution_version , s.memory_capacity FROM IOFHs i INNER JOIN IOFHsRuns ir ON ir.IOFH_id  = i.id INNER JOIN IOFHsScores is2 ON is2.IOFH_id = ir.IOFH_id INNER JOIN sysinfos s  ON s.IOFH_id = is2.IOFH_id;")
+    colname = [d[0] for d in query.description]
+    result_list = [dict(zip(colname, r)) for r in query.fetchall()]
+    cur.close()
+    cur.connection.close()
+    return json.dumps(result_list)
+
+@app.route('/iofh_testcases')
+def iofh_testcases():  # put application's code here
+    cur = get_db().cursor()
+    run_id = request.args.get('run_id')
+    query = cur.execute("SELECT id, name, t_start, exe, stonewall, score, t_delta, t_end FROM IOFHsTestcases WHERE IOFHsRun_id  ="+ run_id+";")
+    colname = [d[0] for d in query.description]
+    result_list = [dict(zip(colname, r)) for r in query.fetchall()]
+    cur.close()
+    cur.connection.close()
+    return json.dumps(result_list)
+
+
+@app.route('/iofh_testcase_results')
+def iofh_testcase_results():  # put application's code here
+    cur = get_db().cursor()
+    run_id = request.args.get('run_id')
+    query = cur.execute("SELECT ires.IOFHsTestcase_id as testcase_id, ires.id as res_id, ires.access, ires.bwMiB, ires.iops, "
+                        "ires.latency, ires.blockKiB, ires.xferKiB, ires.openTime, ires.wrRdTime, ires.closeTime, ires.totalTime, "
+                        "ires.iter, iopt.id  as opt_id, iopt.api, iopt.apiVersion, iopt.testFileName , iopt.access , "
+                        "iopt.\"type\" , iopt.segments , iopt.orderingInaFile, iopt.orderingInterFile, iopt.taskOffset , "
+                        "iopt.nodes, iopt.tasks , iopt.clientsPerNode , iopt.repetitions , iopt.xfersize , iopt.blocksize , "
+                        "iopt.aggregateFilesize , iopt.stonewallingTime , iopt.stoneWallingWearOut  "
+                        "FROM IOFHsResults ires INNER JOIN IOFHsOptions iopt  ON ires.IOFHsTestcase_id  = iopt.IOFHsTestcase_id INNER JOIN IOFHsTestcases it"
+                        "  ON it.id  = iopt.IOFHsTestcase_id where it.IOFHsRun_id == "+run_id+";")
+    colname = [d[0] for d in query.description]
+    result_list = [dict(zip(colname, r)) for r in query.fetchall()]
+    cur.close()
+    cur.connection.close()
+    return json.dumps(result_list)
+
 
 @app.route('/performances')
 def get_performances():  # put application's code here
@@ -108,7 +148,7 @@ def get_multi_summaries_writes():  # put application's code here
     cur = get_db().cursor()
     ids = request.args.get('ids')
     # print(ids)
-    sql = " SELECT * FROM summaries WHERE performance_id in (" + ids +") "
+    sql = " SELECT * FROM summaries WHERE performance_id in (" + ids +")"
     print(sql)
     query = cur.execute(sql)
     colname = [d[0] for d in query.description]
